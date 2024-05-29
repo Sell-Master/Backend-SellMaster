35160be7b012353daed83cb5a61932b7a3f43382
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2024-05-29 17:35:33 -0500

    Prueba

--- product-service/src/main/java/pe/edu/upc/sellmaster/product_service/util/OpenApiConfigs.java
+++ product-service/src/main/java/pe/edu/upc/sellmaster/product_service/util/OpenApiConfigs.java
@@ -1,8 +1,11 @@
 package pe.edu.upc.sellmaster.product_service.util;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,15 +24,15 @@ public class OpenApiConfigs {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .servers(List.of(new Server().url(url)))
-        //        .components(
-        //            new Components()
-        //                .addSecuritySchemes(
-        //                    securitySchemeName,
-        //                    new SecurityScheme()
-        //                        .type(SecurityScheme.Type.HTTP)
-        //                        .scheme("bearer")
-        //                        .bearerFormat("JWT")))
-        //        .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
+                .components(
+                    new Components()
+                        .addSecuritySchemes(
+                            securitySchemeName,
+                            new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                               .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
                 .info(new Info().title(serviceTitle).version(serviceVersion));
     }
 }
