1d438e7d214cfaf8e8efce1b5adc55522ec6c8c9
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2024-05-29 19:01:58 -0500

    Security

--- api-gateway/src/main/java/pe/edu/upc/sellmaster/api_gateway/SecurityConfig.java
+++ api-gateway/src/main/java/pe/edu/upc/sellmaster/api_gateway/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import pe.edu.upc.sellmaster.api_gateway.util.JwtAuthenticationFilter;
+import pe.edu.upc.sellmaster.api_gateway.security.Jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebFluxSecurity
