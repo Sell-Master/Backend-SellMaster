20f73030ec1d57b58c18ca90aa92fd0ad9f00e71
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2024-05-30 02:13:36 -0500

    Security

---
+++ api-gateway/src/main/java/pe/edu/upc/sellmaster/api_gateway/util/RouteValidator.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.sellmaster.api_gateway.util;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/auth/register",
+            "/api/auth/login"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
