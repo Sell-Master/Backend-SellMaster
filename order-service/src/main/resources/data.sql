025085353e600aa8a8502899829ece439972cee0
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2024-06-03 01:21:27 -0500

    Upgrade

--- order-service/src/main/resources/data.sql
+++ order-service/src/main/resources/data.sql
@@ -1,12 +1,14 @@
+-- Inserción de datos en la tabla orders
 INSERT IGNORE INTO orders (fecha, montoTotal, clientID, userID) VALUES
     ('2024-05-31 14:00:00', 150.75, 1, 1),
-    ('2024-05-31 15:00:00', 200.00, 2, 1,
-    ('2024-05-31 16:00:00', 300.50, 3, 1,
+    ('2024-05-31 15:00:00', 200.00, 2, 1),
+    ('2024-05-31 16:00:00', 300.50, 3, 1),
     ('2024-05-31 17:00:00', 100.25, 4, 1);
 
+-- Inserción de datos en la tabla order_details
 INSERT IGNORE INTO order_details (cantidad, productID, orderID) VALUES
-    (5, 101, 1),
-    (3, 102, 1),
-    (2, 103, 2),
-    (4, 104, 3),
-    (1, 101, 4);
+    (5, 1, 1),
+    (3, 2, 1),
+    (2, 3, 2),
+    (4, 4, 3),
+    (1, 1, 4);
